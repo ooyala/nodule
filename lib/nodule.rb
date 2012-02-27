efb733933098753b6b4b9c011886cceca2568038
Al Tobey <al@ooyala.com>
2012-02-27 15:07:23 -0800

    Add automatic requires to nodule.rb.

--- lib/nodule.rb
+++ lib/nodule.rb
@@ -1,5 +1,10 @@
 require "nodule/version"
 
+require "nodule/console"
+require "nodule/monkeypatch"
+require "nodule/process"
+require "nodule/actor"
+require "nodule/topology"
+
 module Nodule
-  # Your code goes here...
 end
