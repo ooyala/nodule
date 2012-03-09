9ff2061cefed76dd849010e4767a5202cd004ca3
Al Tobey <al@ooyala.com>
2012-03-08 16:44:19 -0800

    [test] Add minimal test for ::Tempfile.

--- test/nodule_tempfile_test.rb
+++ test/nodule_tempfile_test.rb
@@ -1,11 +1,25 @@
 #!/usr/bin/env ruby
-$:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
 
-require 'rubygems'
+require_relative 'helper'
 require 'minitest/autorun'
 require 'nodule/tempfile'
 
 class NoduleTempfileTest < MiniTest::Unit::TestCase
-  def test_tempfile
+  def test_basic
+    tfile = nil
+    assert_block do
+      tfile = Nodule::Tempfile.new
+    end
+
+    assert_kind_of Nodule::Tempfile, tfile
+    assert_kind_of Nodule::Actor, tfile
+
+    assert_block do
+      tfile = Nodule::Tempfile.new(:directory => true)
+    end
+    assert_kind_of Nodule::Tempfile, tfile
+    assert_kind_of Nodule::Actor, tfile
+
+    tfile.stop
   end
 end
