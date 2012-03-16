bb56afcbe3e1e666d1ff075b77566dafc357c366
Al Tobey <al@ooyala.com>
2012-03-15 22:57:29 -0700

    [Stdio] add support for run(), done?, etc.
    Now can run readers in background threads. This has some side-effects
    around using the filehandles from outside those threads, but in general
    that's the way it goes with nodule.

--- lib/nodule/stdio.rb
+++ lib/nodule/stdio.rb
@@ -3,13 +3,57 @@ require 'nodule/base'
 module Nodule
   class Stdio < Base
     attr_reader :stdin, :stdout, :stderr
+    attr_reader :stdout_proxy, :stderr_proxy
 
     def initialize(opts={})
+      @threads = []
+      @running = false
+      @handlers = {}
+      @mutex = Mutex.new
+      @stdout_handler = opts.delete :stdout
+      @stderr_handler = opts.delete :stderr
+
       super(opts)
-      @stdin  = opts[:stdin]
-      @stdout = opts[:stdout]
-      @stderr = opts[:stderr]
     end
+
+    #
+    # Create a background thread to read from an IO and call Nodule run_readers.
+    # @param [IO] io
+    # @param [Proc, Nodule::Base] handler
+    #
+    def _run_handler(io, handler)
+      name = io.to_s
+
+      @threads << Thread.new do
+        Thread.current.abort_on_exception = true
+
+        @mutex.synchronize do
+          @handlers[name] = Nodule::Base.new :reader => handler
+          @handlers[name].topology = @topology
+          @handlers[name].run
+        end
+
+        io.each do |line|
+          @handlers[name].run_readers(line)
+        end
+      end
+    end
+
+    def run
+      return if @running
+
+      _run_handler(@stdout, @stdout_handler) if @stdout_handler
+      _run_handler(@stderr, @stderr_handler) if @stderr_handler
+
+      @running = true
+    end
+
+    def stop
+      @handlers.each do |k,h| h.stop end
+      @threads.each do |t| t.join end
+      @running = false
+    end
+    alias :stop! :stop
 
     def wait(timeout=0)      _ready?([@stdout, @stderr], [@stdin], [], timeout) end
     def readable?(timeout=0) _ready?([@stdout,@stderr],  [],       [], timeout) end
@@ -29,12 +73,12 @@ module Nodule
 
     def print(*args)
       raise NotReadyError.new "stdin IO is not ready for writing" unless writable?
-      @stdin.print *args
+      @stdin.print(*args)
     end
 
     def puts(*args)
       raise NotReadyError.new "stdin IO is not ready for writing" unless writable?
-      @stdin.puts *args
+      @stdin.puts(*args)
     end
 
     # might consider the slow/easy path and do 1-byte reads, that way newlines aren't required
@@ -59,11 +103,12 @@ module Nodule
       @stdout.close unless @stdout.nil?
       @stderr.close unless @stderr.nil?
     end
-    alias :stop  :close
-    alias :stop! :close
 
     def done?
-      (@stdin.nil? or @stdin.closed?) and (@stdout.nil? or @stdout.closed?) and (@stderr.nil? or @stderr.closed?)
+      (not @running) and \
+        (@stdin.nil? or @stdin.closed?) and \
+        (@stdout.nil? or @stdout.closed?) and \
+        (@stderr.nil? or @stderr.closed?)
     end
   end
 end
