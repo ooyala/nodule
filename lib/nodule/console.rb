6bf13b5c529c929f172a734058ab882d328fa580
Al Tobey <al@ooyala.com>
2012-03-15 22:54:14 -0700

    [Console] drop rainbow, embed ANSI codes directly
    Rainbow was breaking on binary data because it uses regular expressions
    and was generally clunky anyways.
    
    This also means one less dependency.
    
    The only downside I can see is no more support for arbitrary RGB colors.

--- lib/nodule/console.rb
+++ lib/nodule/console.rb
@@ -1,5 +1,4 @@
 require 'nodule/base'
-require 'rainbow'
 
 module Nodule
   #
@@ -9,39 +8,71 @@ module Nodule
   # Nodule::Console.new(:fg => :green, :bg => :white)
   #
   class Console < Base
+    COLORS = {
+      :black   => "\x1b[30m",
+      :red     => "\x1b[31m",
+      :green   => "\x1b[32m",
+      :yellow  => "\x1b[33m",
+      :blue    => "\x1b[34m",
+      :magenta => "\x1b[35m",
+      :cyan    => "\x1b[36m",
+      :white   => "\x1b[37m",
+      :dkgray  => "\x1b[1;30m",
+      :dkred   => "\x1b[1;31m",
+      :reset   => "\x1b[0m",
+    }.freeze
+
     #
-    # Create a new console actor.
+    # Create a new console src.
     # Color values must be valid for rainbow. See its documentation.
     # @param [Hash] opts
-    # @option [Symbol] opts :fg the foreground color symbol or RGB value
-    # @option [Symbol] opts :bg the background color symbol or RGB value
+    # @option [Symbol] opts :fg the foreground color symbol
+    # @option [Symbol] opts :bg the background color symbol
     #
     def initialize(opts={})
       super(opts)
+      @fg = opts[:fg]
+      @bg = opts[:bg]
 
-      if opts[:fg] and opts[:bg]
-        add_reader { |line,actor| display actor, line.to_s.foreground(opts[:fg]).background(opts[:bg]) }
-      elsif opts[:fg]
-        add_reader { |line,actor| display actor, line.to_s.foreground(opts[:fg]) }
-      elsif opts[:bg]
-        add_reader { |line,actor| display actor, line.to_s.background(opts[:bg]) }
-      else
-        add_reader { |line,actor| display actor, line.to_s }
+      if @fg and not COLORS.has_key?(@fg)
+        raise ArgumentError.new "fg :#{@fg} is not a valid color"
       end
+
+      if @bg and not COLORS.has_key?(@bg)
+        raise ArgumentError.new "bg :#{@bg} is not a valid color"
+      end
+
+      # from https://github.com/sickill/rainbow/blob/master/lib/rainbow.rb
+      @enabled = STDOUT.tty? && ENV['TERM'] != 'dumb' || ENV['CLICOLOR_FORCE'] == '1'
+
+      add_reader { |line,src| display(src, line) }
     end
 
-    private
+    def fg(str)
+      return str unless @enabled
+      "#{COLORS[@fg]}#{str}"
+    end
+
+    def bg(str)
+      return str unless @enabled
+      "#{COLORS[@bg]}#{str}"
+    end
+
+    def reset(str)
+      return str unless @enabled
+      "#{str}#{COLORS[:reset]}"
+    end
 
     #
     # Write to stdout using puts, but append a prefix if it's defined.
-    # @param [Object] actor if this responds to :prefix, :prefix will be prepended to the output
+    # @param [Object] src if this responds to :prefix, :prefix will be prepended to the output
     # @param [String] line the data to write to stdout
     #
-    def display(actor, line)
-      if actor.respond_to? :prefix
-        print "#{actor.prefix}#{line}\n"
+    def display(src, line)
+      if src.respond_to? :prefix
+        print "#{reset('')}#{src.prefix}#{reset(bg(fg(line)))}\n"
       else
-        print "#{line}\n"
+        print "#{reset(bg(fg(line)))}\n"
       end
     end
 
