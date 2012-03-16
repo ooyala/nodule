47f9639e5e7d42a5f46ad1647a42b6e269882aae
Al Tobey <al@ooyala.com>
2012-03-15 22:51:06 -0700

    [Base] arrays of readers, updated wording, _verbose
    :readers can now be an array of readers, also added add_readers
    to complement add_reader.  It takes one or more readers and calls
    add_reader so it's pretty trivial.
    
    Updated topology usage in _verbose.
    
    Fixed wording to continue removing "actor".

--- lib/nodule/base.rb
+++ lib/nodule/base.rb
@@ -12,8 +12,8 @@ module Nodule
     attr_accessor :topology, :prefix
 
     #
-    # Create a new Nodule actor. This is meant to be a bass class for higher-level
-    # Nodule actors.
+    # Create a new Nodule handler. This is meant to be a bass class for higher-level
+    # Nodule types.
     #
     # @param [Hash{Symbol => String,Symbol,Proc}] opts
     # @option opts [String] :prefix text prefix for output/logs/etc.
@@ -29,16 +29,13 @@ module Nodule
       @done    = false
       @topology = nil
 
-      add_reader(opts[:reader]) if opts[:reader]
-      if opts[:readers].respond_to? :each
-        opts[:readers].each do |r| add_reader(r) end
-      end
+      add_readers(opts[:reader]) if opts[:reader]
     end
 
     def run
       @done = false
 
-      # this allows for standalone actors in an automatic one-node topology
+      # this allows for standalone handlers in an automatic one-node topology
       unless @topology
         @toplogy = Nodule::Topology.new(:auto => self)
       end
@@ -65,7 +62,7 @@ module Nodule
     end
 
     #
-    # Wait in a sleep(0.1) loop for the number of reads on the actor to reach <count>.
+    # Wait in a sleep(0.1) loop for the number of reads on the handler to reach <count>.
     # Returns when the number of reads is given. On timeout, if a block was provided,
     # it's called before return. Otherwise, an exception is raised.
     # Has no impact on normal readers.
@@ -94,7 +91,7 @@ module Nodule
     # Add a reader action. Can be a block which will be executed for each unit of input, :capture
     # to capture all items emitted by the target to a list (accessible with .output), :ignore, or
     # nil (which will be ignored).
-    # @param [Symbol, Proc] action Action to take on each item read from the actor
+    # @param [Symbol, Proc] action Action to take on each item read from the handler
     # @option action [Symbol] :capture capture the items into an array (access with .output)
     # @option action [Symbol] :drain read items but throw them away
     # @option action [Symbol] :stderr print the item to stderr (with prefix, in color)
@@ -118,34 +115,54 @@ module Nodule
         # nothing to do here
       # if it's an unrecognized symbol, defer resolution against the containing topology
       elsif action.kind_of? Symbol
-        @readers << proc { |item| @topology[action].run_readers(item, self) }
+        @readers << proc do |item|
+          raise "Topology is not set up!" unless @topology
+          raise ":#{action} is not a valid topology symbol in #{@topology.to_hash.inspect}" unless @topology.has_key?(action)
+          @topology[action].run_readers(item, self)
+        end
       else
         raise ArgumentError.new "Invalid add_reader class: #{action.class}"
       end
     end
 
     #
+    # Add reader arguments with add_reader. Can be a single item or list.
+    # @param [Array<Symbol,Proc,Nodule::Base>] args
+    #
+    def add_readers(*args)
+      args.flatten.each do |reader|
+        add_reader(reader)
+      end
+    end
+
+    #
     # Run all of the registered reader blocks. The block should expect a single argument
     # that is an item of input. If the block has an arity of two, it will also be handed
-    # the actor object provided to run_readers (if it was provided; no guarantee is made that
+    # the nodule object provided to run_readers (if it was provided; no guarantee is made that
     # it will be available). The arity-2 version is provided mostly as a clean way for
     # Nodule::Console to add prefixes to output, but could be useful elsewhere.
     # @param [Object] item the item to pass to the readers, often a String (but could be anything)
-    # @param [Nodule::Actor] actor that generated the item, optional, untyped
+    # @param [Nodule::Base] handler that generated the item, optional, untyped
     #
-    def run_readers(item, actor=nil)
+    def run_readers(item, src=nil)
+      @read_count += 1
+      verbose "READ(#{@read_count}): #{item}"
       @readers.each do |reader|
         if reader.arity == 2
-          reader.call(item, actor)
+          reader.call(item, src)
         else
           reader.call(item)
         end
       end
     end
 
-    def _verbose(*out)
-      if @verbose.respond_to? :run_readers
-        run_readers out.join(' ')
+    #
+    # Verbose Nodule output.
+    # @param [Array<String>] out strings to output, will be joined with ' '
+    #
+    def verbose(*out)
+      if @topology and @topology[@verbose]
+        @topology[@verbose].run_readers out.join(' ')
       elsif @verbose
         STDERR.puts out.join(' ')
       end
