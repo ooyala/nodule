0a65abf882eb81896f4bf5e5e4a2510092078ea0
Al Tobey <al@ooyala.com>
2012-04-27 13:02:25 -0700

    [Base] turns out there are no current users of decay()

--- lib/nodule/base.rb
+++ lib/nodule/base.rb
@@ -132,8 +132,6 @@ module Nodule
         end
       end
     end
-    # temporary 2012-04-27, remove after cleaning up all users of decay()
-    alias :decay :wait_with_backoff
 
     #
     # Wait in a sleep(0.1) loop for the number of reads on the handler to reach <count>.
