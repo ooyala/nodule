dce2caa66d9c9826cf807e861521bee07ec54906
Noah Gibbs <noah@ooyala.com>
2012-06-07 15:17:24 -0700

    Add first draft Jenkins run.sh for nodule tests

---
+++ ci_jobs/nodule-units/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+
+# Disallow errors
+set -e
+
+if [[ -z $WORKSPACE ]] ; then
+  # Support execution from the shell
+  export PROJECT_DIR=$(pwd);
+else
+  export PROJECT_DIR=$WORKSPACE/glowworm;
+fi
+
+cd $(PROJECT_DIR)
+
+echo START TASK: tests
+bundle exec rake test
+echo END TASK
